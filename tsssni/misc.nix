26c125ec14cc7b6dd6a011a6c4a6e3690ad713c9
tsssni <dingyongyu2002@foxmail.com>
2024-03-17 03:06:50 +0800

    add google-chrome

--- tsssni/misc.nix
+++ tsssni/misc.nix
@@ -3,4 +3,16 @@
 	home.packages = with pkgs; [
 		(nerdfonts.override { fonts = [ "Monaspace" ]; })
 	];
+
+	programs = {
+		chromium = {
+			enable = true;
+			package = pkgs.google-chrome;
+			commandLineArgs = [ 
+				"--enable-features=UseOzonePlatform"
+				"--ozone-platform=wayland" 
+				"--enable-wayland-ime" 
+			];
+		};
+	};
 }
