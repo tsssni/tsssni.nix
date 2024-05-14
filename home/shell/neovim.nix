f978f4a22213d06c5d19c3bb3027487691556810
tsssni <dingyongyu2002@foxmail.com>
2024-05-15 00:30:04 +0800

    install lsp with nix

--- home/shell/neovim.nix
+++ home/shell/neovim.nix
@@ -1,10 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 {
-	programs.neovim = {
-		enable = true;
-		defaultEditor = true;
-	};
+	programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+    # for telescope
+    ripgrep.enable = true;
+  };
 
-  # for telescope
-  programs.ripgrep.enable = true;
+  home.packages = with pkgs; [
+    # lsp
+    clang-tools
+    cmake-language-server
+    lua-language-server
+    nil
+    nodePackages.typescript-language-server
+    texlab
+    # dap
+    lldb
+  ];
 }
