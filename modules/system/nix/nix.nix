708cc6b1fd98e29cf25089a1c2e4e229516c635d
tsssni <dingyongyu2002@foxmail.com>
2025-03-09 16:55:02 +0800

    fix darwin store optimization

--- modules/system/nix/nix.nix
+++ modules/system/nix/nix.nix
@@ -3,15 +3,17 @@
 , ...
 }:
 {
-	nix.settings = {
-		experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
-		auto-optimise-store = true;
-		substituters = [
-			"https://cache.garnix.io"
-		];
-		trusted-public-keys = [
-			"cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-		];
+	nix = {
+		settings = {
+			experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
+			substituters = [
+				"https://cache.garnix.io"
+			];
+			trusted-public-keys = [
+				"cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+			];
+		};
+		optimise.automatic = true;
 	};
 	nixpkgs = {
 		hostPlatform = tsssni.system;
