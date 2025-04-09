11036ad74af61c73271d03b29216d747dae1735f
tsssni <dingyongyu2002@foxmail.com>
2025-04-10 02:08:15 +0800

    remove duplicated history and add omz for darwin

--- configs/nix-darwin/mba/tsssni/shell/zsh.nix
+++ configs/nix-darwin/mba/tsssni/shell/zsh.nix
@@ -5,18 +5,24 @@
 {
 	programs.zsh = {
 		enable = true;
+		oh-my-zsh = {
+			enable = true;
+			theme = "";
+		};
 		syntaxHighlighting.enable = true;
-		historySubstringSearch.enable = true;
-		autosuggestion.enable = true;
+        historySubstringSearch.enable = true;
+        autosuggestion.enable = true;
 		history = {
 			size = 114514;
 			save = 114514;
+			ignoreAllDups = true;
+			saveNoDups = true;
 		};
 		shellAliases = {
 			fastfetch = "DYLD_LIBRARY_PATH=${pkgs.darwin.moltenvk}/lib fastfetch";
 		};
-		initExtra = ''
-			export EDITOR="nvim"
-		'';
+		sessionVariables = {
+			EDITOR = "nvim";
+		};
 	};
 }
