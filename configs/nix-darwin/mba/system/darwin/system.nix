c5977a4140d50de59d38e90f328cd1d81d4a129a
tsssni <dingyongyu2002@foxmail.com>
2025-05-03 00:05:02 +0800

    add known user for nix-darwin to change login shell

--- configs/nix-darwin/mba/system/darwin/system.nix
+++ configs/nix-darwin/mba/system/darwin/system.nix
@@ -3,10 +3,14 @@
 , ...
 }: 
 {
-	users.users.tsssni = {
-		name = "tsssni";
-		home = "/Users/tsssni";
-		shell = config.tsssni.shell.shell.package;
+	users = {
+		knownUsers = [ "tsssni" ];
+		users.tsssni = {
+			name = "tsssni";
+			home = "/Users/tsssni";
+			shell = config.tsssni.shell.shell.package;
+			uid = 501;
+		};
 	};
 
 	system.stateVersion = 6;
