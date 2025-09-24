da796b34d191887b07e6065459ea0caf3547b17b
tsssni <dingyongyu2002@foxmail.com>
2025-09-24 10:49:39 +0800

    user: kitty larger font on gnome

--- configs/home-manager/user/shell.nix
+++ configs/home-manager/user/shell.nix
@@ -4,6 +4,9 @@
     fetch.enable = true;
     prompt.enable = true;
     shell.enable = true;
-    terminal.enable = true;
+    terminal = {
+      enable = true;
+      extraSettings.font_size = 20.0;
+    };
   };
 }
