8784430a18f25ac2f101fed8bb6effaa5185f0d2
tsssni <dingyongyu2002@foxmail.com>
2025-10-18 17:34:40 +0800

    nix: add nix-community cache

--- configs/prelude/nix.nix
+++ configs/prelude/nix.nix
@@ -5,13 +5,9 @@
   ...
 }:
 {
-  nix = {
-    package = pkgs.nix;
-    nixPath = [
-      "nixpkgs=${tsssni.inputs.nixpkgs}"
-    ];
-    settings = {
-      experimental-features = [
+  nix =
+    let
+      features = [
         "nix-command"
         "flakes"
         "pipe-operators"
@@ -19,34 +15,44 @@
       substituters = [
         "https://cache.nixos.org"
         "https://cache.garnix.io"
+        "https://nix-community.cachix.org"
       ];
-      trusted-substituters = [
-        "https://cache.garnix.io"
-      ];
-      trusted-public-keys = [
+      keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-    };
-  }
-  // lib.optionalAttrs (tsssni.distro != "home") {
-    gc = {
-      automatic = true;
-      options = "--delete-older-than 7d";
-    }
-    // lib.optionalAttrs pkgs.stdenv.isLinux {
-      persistent = true;
-      dates = "Tue 04:00";
-    }
-    // lib.optionalAttrs pkgs.stdenv.isDarwin {
-      interval = {
-        Weekday = 2;
-        Hour = 4;
-        Minute = 0;
+    in
+    {
+      package = pkgs.nix;
+      nixPath = [
+        "nixpkgs=${tsssni.inputs.nixpkgs}"
+      ];
+      settings = {
+        experimental-features = features;
+        substituters = substituters;
+        trusted-substituters = substituters;
+        trusted-public-keys = keys;
       };
+    }
+    // lib.optionalAttrs (tsssni.distro != "home") {
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 7d";
+      }
+      // lib.optionalAttrs pkgs.stdenv.isLinux {
+        persistent = true;
+        dates = "Tue 04:00";
+      }
+      // lib.optionalAttrs pkgs.stdenv.isDarwin {
+        interval = {
+          Weekday = 2;
+          Hour = 4;
+          Minute = 0;
+        };
+      };
+      optimise.automatic = true;
     };
-    optimise.automatic = true;
-  };
 }
 // (
   let
