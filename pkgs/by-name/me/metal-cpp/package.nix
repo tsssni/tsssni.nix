1a76a7407ea87c3fbd15ccefda8078b1c70cb5d9
tsssni <dingyongyu2002@foxmail.com>
2025-12-31 20:34:11 +0800

    metal-cpp: add

---
+++ pkgs/by-name/me/metal-cpp/package.nix
@@ -0,0 +1,32 @@
+{
+  fetchzip,
+  stdenv,
+  lib,
+  apple-sdk_26,
+  cmake,
+}:
+stdenv.mkDerivation rec {
+  pname = "metal-cpp";
+  version = "26";
+
+  src = fetchzip {
+    url = "https://developer.apple.com/metal/cpp/files/metal-cpp_${version}.zip";
+    hash = "sha256-7n2eI2lw/S+Us6l7YPAATKwcIbRRpaQ8VmES7S8ZjY8=";
+    stripRoot = true;
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ apple-sdk_26 ];
+
+  postUnpack = ''
+    cp ${./CMakeLists.txt} $sourceRoot/CMakeLists.txt                               
+    cp ${./metal-cpp-config.cmake.in} $sourceRoot/metal-cpp-config.cmake.in         
+  '';
+
+  meta = {
+    description = "Metal-cpp is a low-overhead C++ interface for Metal that helps you add Metal functionality to graphics apps, games, and game engines that are written in C++.";
+    homepage = "https://developer.apple.com/metal/cpp/";
+    license = lib.licenses.asl20;
+    platforms = lib.platforms.darwin;
+  };
+}
