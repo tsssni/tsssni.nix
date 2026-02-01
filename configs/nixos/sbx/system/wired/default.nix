ecaa892fc5bf21ef550a6784703abe8970a3a13c
tsssni <dingyongyu2002@foxmail.com>
2026-02-01 14:46:03 +0800

    config: remove unused ones

--- configs/nixos/sbx/system/wired/default.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  imports = [
-    ./sing-box.nix
-    ./ssh.nix
-    ./system.nix
-    ./tls.nix
-  ];
-}
--- configs/prelude.nix
+++ configs/prelude.nix
@@ -12,13 +12,15 @@ let
   ];
   substituters = [
     "https://cache.nixos.org"
-    "https://cache.garnix.io"
+    "https://cache.nixos-cuda.org"
     "https://nix-community.cachix.org"
+    "https://cache.garnix.io"
   ];
   keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
   packages = with pkgs; [
     nix
