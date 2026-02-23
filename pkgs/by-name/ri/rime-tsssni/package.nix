32b156fcc4b19a53d6cf9f39597034bb34e89fae
tsssni <dingyongyu2002@foxmail.com>
2026-02-24 00:09:27 +0800

    squirrel: add

--- pkgs/by-name/ri/rime-tsssni/package.nix
+++ pkgs/by-name/ri/rime-tsssni/package.nix
@@ -19,6 +19,7 @@ buildEnv {
   postBuild = ''
     ln -s ${./rime_ice.custom.yaml} $out/share/rime-data/rime_ice.custom.yaml
     ln -s ${./default.custom.yaml} $out/share/rime-data/default.custom.yaml
+    ln -s ${./squirrel.custom.yaml} $out/share/rime-data/squirrel.custom.yaml
 
     cd $out/share/rime-data/
     for s in *.schema.yaml; do
