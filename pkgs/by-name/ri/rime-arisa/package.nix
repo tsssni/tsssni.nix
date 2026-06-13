e9bac4b2e20e8484f18026f874934e66fa41b66b
tsssni <dingyongyu2002@foxmail.com>
2026-06-14 01:37:05 +0800

    rime: fix error of ice

--- pkgs/by-name/ri/rime-arisa/package.nix
+++ pkgs/by-name/ri/rime-arisa/package.nix
@@ -20,12 +20,13 @@ buildEnv {
     ln -s ${./rime_ice.custom.yaml} $out/share/rime-data/rime_ice.custom.yaml
     ln -s ${./default.custom.yaml} $out/share/rime-data/default.custom.yaml
     ln -s ${./squirrel.custom.yaml} $out/share/rime-data/squirrel.custom.yaml
-
     cd $out/share/rime-data/
+
+    rm -f ./build
+    mkdir -p ./build
     for s in *.schema.yaml; do
       rime_deployer --compile "$s" . "${rime-data}/share/rime-data/" ./build
     done
-
-    rm ./build/*.txt
+    find ./build -name '*.txt' -delete
   '';
 }
